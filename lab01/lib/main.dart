b35a6218e431ebb18b1a403d60eff9d976150160
NianchurAngelina <angelinanianchur@gmail.com>
2023-10-31 11:28:51 +0200

    adaptive

--- lab01/lib/main.dart
+++ lab01/lib/main.dart
@@ -17,13 +17,17 @@ class MyApp extends StatelessWidget {
 class OwnCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    double screenWidth = MediaQuery.of(context).size.width;
+    double screenHeight = MediaQuery.of(context).size.height;
+    bool isLandscape = screenWidth > screenHeight;
+
     return Scaffold(
       appBar: AppBar(
         title: Text('Own Card'),
       ),
       body: Center(
         child: Container(
-          padding: EdgeInsets.all(20.0),
+          padding: EdgeInsets.all(isLandscape ? 40.0 : 20.0),
           decoration: BoxDecoration(
             gradient: LinearGradient(
               colors: [Color.fromRGBO(4, 250, 25, 1), Colors.blueAccent],
@@ -34,28 +38,65 @@ class OwnCard extends StatelessWidget {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
-              CircleAvatar(
-                radius: 60.0,
-                backgroundImage: FileImage(File('img/1.jpg')),
-              ),
-              SizedBox(height: 20.0),
-              Text(
-                'Nianchur Angelina',
-                style: TextStyle(
-                  fontSize: 26.0,
-                  fontWeight: FontWeight.bold,
-                  color: Color.fromARGB(255, 0, 0, 0),
+              if (isLandscape)
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    CircleAvatar(
+                      radius: 120.0,
+                      backgroundImage: FileImage(File('img/1.jpg')),
+                    ),
+                    SizedBox(width: 20.0),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Nianchur Angelina',
+                          style: TextStyle(
+                            fontSize: 36.0,
+                            fontWeight: FontWeight.bold,
+                            color: Color.fromARGB(255, 0, 0, 0),
+                          ),
+                        ),
+                        SizedBox(height: 10.0),
+                        Text(
+                          'Student of Ogienko university',
+                          style: TextStyle(
+                            fontSize: 20.0,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
                 ),
-              ),
-              SizedBox(height: 10.0),
-              Text(
-                'Student of Ogienko university',
-                style: TextStyle(
-                  fontSize: 15.0,
-                  color: Colors.white,
+              if (!isLandscape)
+                Column(
+                  children: [
+                    CircleAvatar(
+                      radius: 60.0,
+                      backgroundImage: FileImage(File('img/1.jpg')),
+                    ),
+                    SizedBox(height: 20.0),
+                    Text(
+                      'Nianchur Angelina',
+                      style: TextStyle(
+                        fontSize: 26.0,
+                        fontWeight: FontWeight.bold,
+                        color: Color.fromARGB(255, 0, 0, 0),
+                      ),
+                    ),
+                    SizedBox(height: 10.0),
+                    Text(
+                      'Student of Ogienko university',
+                      style: TextStyle(
+                        fontSize: 15.0,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ],
                 ),
-              ),
-              SizedBox(height: 20.0),
+              SizedBox(height: isLandscape ? 40.0 : 20.0),
               Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: <Widget>[
@@ -64,13 +105,13 @@ class OwnCard extends StatelessWidget {
                   Text(
                     'kn1b21.nianchur@kpnu.edu.ua',
                     style: TextStyle(
-                      fontSize: 12.0,
+                      fontSize: isLandscape ? 12.0 : 8.0,
                       color: Colors.white,
                     ),
                   ),
                 ],
               ),
-              SizedBox(height: 10.0),
+              SizedBox(height: isLandscape ? 20.0 : 10.0),
               Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: <Widget>[
@@ -79,7 +120,7 @@ class OwnCard extends StatelessWidget {
                   Text(
                     '0965951600',
                     style: TextStyle(
-                      fontSize: 12.0,
+                      fontSize: isLandscape ? 12.0 : 8.0,
                       color: Colors.white,
                     ),
                   ),
--- lab02/lib/main.dart
+++ lab02/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'dart:io'; //для можливості використовувати File Image
+import 'dart:io';
 
 void main() {
   runApp(MyApp());
@@ -24,56 +24,67 @@ class HomeScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text('Лабораторна робота №2, перегляд картинок як слайдер ;)'),
       ),
-      body: Column(
-        children: [
-          Text(
-            'Переглядач картинок: Funny animamls',
-            style: TextStyle(fontSize: 26),
-            textAlign: TextAlign.center,
-          ),
-          Image.file(File('images/10.jpg'), width: 500, height: 350),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: [
-              ElevatedButton(
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) =>
-                          CategoryScreen('dogs', 'images/dogs'),
-                    ),
-                  );
-                },
-                child: Text('dogs'),
+      body: LayoutBuilder(
+        builder: (context, constraints) {
+          return SingleChildScrollView(
+            child: Center(
+              child: Column(
+                children: [
+                  SizedBox(height: 20),
+                  Text(
+                    'Переглядач картинок: Funny animals',
+                    style: TextStyle(fontSize: 26),
+                    textAlign: TextAlign.center,
+                  ),
+                  SizedBox(height: 20),
+                  Image.file(File('images/10.jpg'), width: constraints.maxWidth, height: constraints.maxHeight * 0.4),
+                  SizedBox(height: 20),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      ElevatedButton(
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) =>
+                                  CategoryScreen('dogs', 'images/dogs'),
+                            ),
+                          );
+                        },
+                        child: Text('dogs'),
+                      ),
+                      ElevatedButton(
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) =>
+                                  CategoryScreen('cats', 'images/cats'),
+                            ),
+                          );
+                        },
+                        child: Text('cats'),
+                      ),
+                      ElevatedButton(
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) =>
+                                  CategoryScreen('rabbits', 'images/rabbits'),
+                            ),
+                          );
+                        },
+                        child: Text('rabbits'),
+                      ),
+                    ],
+                  ),
+                ],
               ),
-              ElevatedButton(
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) =>
-                          CategoryScreen('cats', 'images/cats'),
-                    ),
-                  );
-                },
-                child: Text('cats'),
-              ),
-              ElevatedButton(
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) =>
-                          CategoryScreen('rabbits', 'images/rabbits'),
-                    ),
-                  );
-                },
-                child: Text('rabbits'),
-              ),
-            ],
-          ),
-        ],
+            ),
+          );
+        },
       ),
     );
   }
@@ -91,7 +102,7 @@ class CategoryScreen extends StatefulWidget {
 
 class _CategoryScreenState extends State<CategoryScreen> {
   int currentImageIndex = 1;
-  int totalImages = 3; // Загальна кількість фотографій в папці
+  int totalImages = 3;
 
   void showNextImage() {
     if (currentImageIndex < totalImages) {
@@ -115,37 +126,43 @@ class _CategoryScreenState extends State<CategoryScreen> {
       appBar: AppBar(
         title: Text(widget.categoryName),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Image.file(
-              File(
-                  '${widget.imagePath}/${widget.categoryName.toLowerCase()}-$currentImageIndex.jpeg'),
-              width: 500,
-              height: 350,
+      body: LayoutBuilder(
+        builder: (context, constraints) {
+          return SingleChildScrollView(
+            child: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Image.file(
+                    File('${widget.imagePath}/${widget.categoryName.toLowerCase()}-$currentImageIndex.jpeg'),
+                    width: constraints.maxWidth,
+                    height: constraints.maxHeight * 0.4,
+                  ),
+                  SizedBox(height: 20),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      IconButton(
+                        onPressed: showPreviousImage,
+                        icon: Icon(Icons.arrow_back),
+                      ),
+                      IconButton(
+                        onPressed: showNextImage,
+                        icon: Icon(Icons.arrow_forward),
+                      ),
+                    ],
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                    child: Text('На головну'),
+                  ),
+                ],
+              ),
             ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                IconButton(
-                  onPressed: showPreviousImage,
-                  icon: Icon(Icons.arrow_back),
-                ),
-                IconButton(
-                  onPressed: showNextImage,
-                  icon: Icon(Icons.arrow_forward),
-                ),
-              ],
-            ),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.pop(context);
-              },
-              child: Text('На головну'),
-            ),
-          ],
-        ),
+          );
+        },
       ),
     );
   }
